e0e4a37b6d28d8849a6d49c187ff823106bb7d18
juniusvariant <juniusvarianto@gmail.com>
2021-06-14 17:59:39 +0700

    update config app

--- configs/app_config.py
+++ configs/app_config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    APP_MODE: str = "Development" ##value -> Development / Production
+    APP_MODE: str = "Production" ##value -> Development / Production
     INET_MODE: str = "Online" ##value -> Online / Offline
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: str = "HS256"
