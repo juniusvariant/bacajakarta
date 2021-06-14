a74c21ccfa3124c9d2760bfedc4ae0828a947124
juniusvariant <juniusvarianto@gmail.com>
2021-06-14 14:04:58 +0700

    update config app

--- configs/db_config.py
+++ configs/db_config.py
@@ -25,7 +25,7 @@ app_mode = conf.APP_MODE
 if app_mode == 'Development':
     SQLALCHEMY_DATABASE_URL =  "postgresql+asyncpg://juniusvariant@127.0.0.1:5432/bacajakarta_db"
 if app_mode == 'Production':
-    SQLALCHEMY_DATABASE_URL =  "postgres+asyncpg://usthyfbohhvykd:2ff06dd828917f5f8f0fef027d11d4337bef1815a91a84b176a0f86ae44b6377@ec2-35-171-250-21.compute-1.amazonaws.com:5432/depk1bbdajorbf"
+    SQLALCHEMY_DATABASE_URL =  "postgresql+asyncpg://usthyfbohhvykd:2ff06dd828917f5f8f0fef027d11d4337bef1815a91a84b176a0f86ae44b6377@ec2-35-171-250-21.compute-1.amazonaws.com:5432/depk1bbdajorbf"
 
 engine =  create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
